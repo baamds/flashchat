89f9ea8aa4b81522ff2e727eb650518672401c3f
baamds <amirmobili7@gmail.com>
2026-08-13 01:57:13 +0200

    MessageCell configured

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -36,23 +36,24 @@ class ChatViewController: UIViewController {
     }
     // add a snapshot listener to database
     func loadMessages() {
-          db.collection(K.FStore.collectionName)
+        db.collection(K.FStore.collectionName)
             .order(by: K.FStore.dateField)
             .addSnapshotListener { querySnapshot, error in
-            self.messages = []
-            if let snapshotDocuments = querySnapshot?.documents {
-                for doc in snapshotDocuments {
-                    let data = doc.data()
-                    if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
-                        let message = Message(sender: messageSender, body: messageBody)
-                        self.messages.append(message)
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
+                self.messages = []
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let message = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(message)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                self.tableView.scrollToRow(at: IndexPath(row: self.messages.count - 1, section: 0), at: .top, animated: true)
+                            }
                         }
                     }
                 }
             }
-        }
     }
     
     
@@ -66,6 +67,9 @@ class ChatViewController: UIViewController {
                     
                 } else {
                     print("Successfully saved Data")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
                 }
             }
         }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -17,4 +17,14 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         titleLabel.text = "⚡️FlashChat"
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 }
